e77dea6ed8eee8dff7bf35462a57e19dd3533355
Mariuccifilippo <mariuccifilippo@outlook.it>
2021-01-05 16:40:29 +0100

    Initial commit

---
+++ src/main/java/com/example5/MyRunner.java
@@ -0,0 +1,26 @@
+package com.example5;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+import javax.transaction.Transactional;
+import java.util.logging.Logger;
+
+public class MyRunner implements CommandLineRunner {
+    private static final Logger logger= (Logger) LoggerFactory.getLogger(MyRunner.class);
+
+    @Autowired
+    private MeteoRepository meteoRepository;
+
+    @Override
+    @Transactional
+
+    public void run(String... args) throws Exception {
+        logger.info("inizializer users");
+
+        var u1= new SpazioVariabili("3173435","it",278.71,279.82,279.26,275.98,"Milano");
+        meteoRepository.save(u1);
+
+    }
+}
